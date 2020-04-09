f7ae217f38457bd7e4e21dc3fa12354c9892bc14
Igor Guilherme <49598006+IgorLoredo@users.noreply.github.com>
2020-04-08 23:35:26 -0300

    test for class Dado
    testei se a class dados esta funcionando
    pelo visto ta otima

--- bozo/src/bozo/Dado.java
+++ bozo/src/bozo/Dado.java
@@ -4,7 +4,9 @@ public class Dado {
 	private int lados;
 	private Random rand;
 	private int LadoCima;
-	public int rolar() {
+	
+	
+	public int rolar() { // usa o rand e rola os dados de forma efiente
 		try {
 			LadoCima = rand.getIntRand(1, lados+1); // usa a função random
 		} catch(IllegalArgumentException e) {
@@ -17,7 +19,7 @@ public class Dado {
 	public Dado() { // defeat com 6 lados
 		lados = 6;
 		rand = new Random();
-		rolar();
+		rolar(); // rola os dados
 	}
 	
 	public Dado(int n) { // construtor que usar mais/menos de 6 lados
@@ -30,12 +32,12 @@ public class Dado {
 	
 	public int getLado() {
 		
-		return LadoCima;
+		return LadoCima; // retona lado sorteado pelo o rand
 	}
 	
 	public String toString() {
 		String visaolados = " ";
 		
-		return visaolados;
+		return visaolados; 
 	}
 }
--- bozo/src/bozo/bozo.java
+++ bozo/src/bozo/bozo.java
@@ -8,6 +8,10 @@ public class bozo {
 
 	public static void main(String[] args) {
 		
+		Dado igor = new Dado();
+		igor.rolar();
+		int lado = igor.getLado();
+		System.out.println(lado);
 		
 
 	}
