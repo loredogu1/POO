0aa5d6807d2ca31d74c77d22561c68bbf78d8566
Igor Guilherme <49598006+IgorLoredo@users.noreply.github.com>
2020-04-12 22:48:27 -0300

    teste para ir

--- bozo/src/bozo/RolaDados.java
+++ bozo/src/bozo/RolaDados.java
@@ -27,5 +27,56 @@ public class RolaDados {
     }
     
     
+    public int [] rolar (String s) {
+        int resultado[], auxiliar[];
+        resultado = new int[quantLados];
+        String separados[];
+        separados = s.split(" ");
+        int tam = separados.length;
+        auxiliar = new int[tam];
+        for (int i = 0; i < tam; i++) {
+            auxiliar[i] = Integer.parseInt(separados[i]) - 1;
+        }
+
+        for (int i = 0; i < tam; i++) {
+            dados[auxiliar[i]].rolar();
+        }
+
+        for (int i = 0; i <quantLados; i++) {
+            resultado[i] = dados[i].getLado();
+        }
+        return resultado;
+    }
+    
+    public String toString () {
+
+        String result = " 1        2        3        4        5\n+-----+  +-----+  +-----+  +-----+  +-----+\n";
+        for(int i=0; i< 5; i++) {
+            if (dados[i].getLado() == 1) result +="|     |  ";
+            else if (dados[i].getLado() == 2 || dados[i].getLado() == 3) result+="|*    |  ";
+            else result+="|*   *|  ";
+            if (i == 4) result+="\n";
+        }
+
+        for(int i=0; i< 5; i++) {
+            if (dados[i].getLado() == 1 || dados[i].getLado() == 3 || dados[i].getLado() == 5) result+="|  *  |  ";
+            else if (dados[i].getLado() == 6) 
+            	result+="|*   *|  ";
+            else
+            	result+="|     |  ";
+            if (i == 4) result+="\n";
+        }
+
+        for(int i=0; i< 5; i++) {
+            if (dados[i].getLado() == 2 || dados[i].getLado() == 3) result+="|    *|  ";
+            else if (dados[i].getLado() == 1) result+="|     |  ";
+            else result+="|*   *|  ";
+            if (i == 4) result+="\n";
+        }
+
+        result+="+-----+  +-----+  +-----+  +-----+  +-----+\n";
+        return result;
+    }
+    
     
 }
--- bozo/src/bozo/bozo.java
+++ bozo/src/bozo/bozo.java
@@ -2,11 +2,11 @@
 
 package bozo;
 
-
+import java.io.IOException;
 
 public class bozo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		RolaDados vetdados = new RolaDados(5);
 		Placar placar = new Placar();
 		
@@ -15,9 +15,59 @@ public class bozo {
 		System.out.println("_________________________________");
 		
 		for(int i = 0; i < 10; i++) {
-			System.out.println("");
+			System.out.println("Aperte Enter para começar a rodada");
+			
+			String buffer = EntradaTeclado.leString();
+			
+			int result[] = vetdados.rolar(); // imprime		            
+            System.out.println(vetdados);
+			System.out.printf("Escolha os dados para rolar novamente ou digite ENTER para terminar a  rodada\n");
+			
+			
+            String novos = EntradaTeclado.leString();		
+			
+            
+            if (!novos.equals("")) {
+                result = vetdados.rolar(novos);
+                System.out.println(vetdados);
+
+                System.out.printf("Escolha os dados a serem rolados novamente ou digite ENTER para finalizar a rodada\n");
+                novos = EntradaTeclado.leString();
+
+                if (!novos.equals("")) {
+
+                    result = vetdados.rolar(novos);
+
+                }
+                System.out.println(vetdados);
+            } else
+            	System.out.println(vetdados);
+            
+            
+            System.out.println(placar);
+            System.out.printf("Escolha uma posicao de 1 a 10 para ser ocupada\n");
+            int pos = EntradaTeclado.leInt();
+
+            try {
+                placar.add( result, pos);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Escolha uma posicao possivel para ser ocupada");
+                pos = EntradaTeclado.leInt();
+                placar.add(result, pos);
+            }
+                System.out.println(placar);
+            
+			
 		}
+		
+		System.out.println("Você obteve " + placar.getScore() + " pontos!\n");
 
 	}
 
 }
+
+
+
+
+
